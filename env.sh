1e50866dcc9b0f738a30cd3b26b1644bab5e1bdc
Elliot Cameron <eacameron@gmail.com>
2018-11-13 16:50:12 -0500

    Add some tools and move nixpkgs config to its own file

--- env.sh
+++ env.sh
@@ -27,6 +27,7 @@ function user-packages() {
   echo git
   echo git-crypt
   echo gitAndTools.hub
+  echo gitkraken
   echo gnugrep
   echo gnumake
   echo gnupg
@@ -38,18 +39,20 @@ function user-packages() {
   echo haskellPackages.stack
   echo haskellPackages.stylish-haskell
   echo htop
+  echo imagemagick7
   echo inotify-tools
   echo jq
   echo kdiff3
-  echo keybase
-  echo keybase-gui
   echo lastpass-cli
   echo less
   echo libreoffice
+  echo nix-prefetch-github
   echo nix-prefetch-scripts
+  echo obelisk
   echo p7zip
   echo psmisc
   echo shellcheck
+  echo signal-desktop
   echo spectacle
   echo steam
   echo thunderbird
@@ -87,37 +90,9 @@ function vscode-extensions() {
 
 
 function user-nix-config() {
-  cat <<NIX
-{
-  allowUnfree = true;
-
-  packageOverrides = pkgs: {
-    haskellPackages-mine = pkgs.haskellPackages.override {
-      overrides = self: super: {
-        #haskell-src-exts-HEAD = self.callCabal2nix "haskell-src-exts" (pkgs.fetchFromGitHub {
-        #  owner  = "haskell-suite";
-        #  repo   = "haskell-src-exts";
-        #  rev    = "935f6f0915e89c314b686bdbdc6980c72335ba3c";
-        #  sha256 = "1v3c1bd5q07qncqfbikvs8h3r4dr500blm5xv3b4jqqv69f0iam9";
-        #}) {};
-        #haskell-src-exts = self.haskell-src-exts-HEAD;
-
-        #hlint = super.hlint.overrideAttrs (_: { haskell-src-exts = self.haskell-src-exts-HEAD; });
-        #stylish-haskell = super.stylish-haskell.overrideAttrs (_: { haskell-src-exts = self.haskell-src-exts-HEAD; });
-
-        twitch-cli = self.callCabal2nix "twitch-cli" (pkgs.fetchFromGitHub {
-          owner  = "grafted-in";
-          repo   = "twitch-cli";
-          rev    = "85047186c3790ab8f015bdc4658abfe63c6129b7";
-          sha256 = "1yr53r3h0p12dj2cyc3j6r71nyf0g93x1xbra9205f6qp3ymc205";
-        }) {};
-
-        #intero = pkgs.haskell.lib.dontCheck super.intero;
-        #stylish-haskell = pkgs.haskell.lib.doJailbreak super.stylish-haskell;
-      };
-    };
-  };
-}
+  mkdir -p "$HOME/.config/nixpkgs"
+  cat > "$HOME/.config/nixpkgs/config.nix" <<NIX
+import "$env_script_path/nixpkgs-config.nix"
 NIX
 }
 
---
+++ nixpkgs-config.nix
@@ -0,0 +1,32 @@
+{
+  allowUnfree = true;
+
+  packageOverrides = pkgs: {
+    obelisk = (import dep/obelisk {}).command;
+
+    haskellPackages-mine = pkgs.haskellPackages.override {
+      overrides = self: super: {
+        #haskell-src-exts-HEAD = self.callCabal2nix "haskell-src-exts" (pkgs.fetchFromGitHub {
+        #  owner  = "haskell-suite";
+        #  repo   = "haskell-src-exts";
+        #  rev    = "935f6f0915e89c314b686bdbdc6980c72335ba3c";
+        #  sha256 = "1v3c1bd5q07qncqfbikvs8h3r4dr500blm5xv3b4jqqv69f0iam9";
+        #}) {};
+        #haskell-src-exts = self.haskell-src-exts-HEAD;
+
+        #hlint = super.hlint.overrideAttrs (_: { haskell-src-exts = self.haskell-src-exts-HEAD; });
+        #stylish-haskell = super.stylish-haskell.overrideAttrs (_: { haskell-src-exts = self.haskell-src-exts-HEAD; });
+
+        twitch-cli = self.callCabal2nix "twitch-cli" (pkgs.fetchFromGitHub {
+          owner  = "grafted-in";
+          repo   = "twitch-cli";
+          rev    = "85047186c3790ab8f015bdc4658abfe63c6129b7";
+          sha256 = "1yr53r3h0p12dj2cyc3j6r71nyf0g93x1xbra9205f6qp3ymc205";
+        }) {};
+
+        #intero = pkgs.haskell.lib.dontCheck super.intero;
+        #stylish-haskell = pkgs.haskell.lib.doJailbreak super.stylish-haskell;
+      };
+    };
+  };
+}
